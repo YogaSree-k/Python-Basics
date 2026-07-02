f88847c799571924fa066f65dadea628825bab45
Yoga Sree Kante <sreekante14@gmail.com>
2026-07-03 00:33:05 +0530

    Day 1 - Python Strings

---
+++ day1_strings.py
@@ -0,0 +1,8 @@
+name = "Yoga Sree"
+
+print("Uppercase:", name.upper())
+print("Lowercase:", name.lower())
+print("First letter:", name[0])
+print("Last letter:", name[-1])
+print("Length:", len(name))
+print(f"Welcome to Python, {name}!")
